689f0186196e4c4b4850f6a88fd4be9fa591fab4
RingCentral Integrations Team <integrations@ringcentral.com>
2017-03-22 15:38:16 +0800

    released at Wed Mar 22 15:38:16 CST 2017

---
+++ containers/MessagesPage/i18n/en-GB.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = {
+  title: 'Messages',
+  search: 'Search...',
+  noMessages: 'No messages',
+  noSearchResults: 'No matching records found'
+};
+//# sourceMappingURL=en-GB.js.map
