8503ac1008a4e34705a46782526aa15c21deff80
embbnux <embbnux@embbnux.com>
2017-05-29 23:47:39 +0800

    released at Mon May 29 23:47:39 CST 2017

--- services/rc-config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    key: '8mOtYiilT5OUPwwdeGgvpw',
-    secret: 'cqNn89RmR2SR76Kpp8xJaAdNzNOqR8Qfmjb0B-gDOHTw',
-
-    incomingAudio: '../build/assets/audio/incoming.ogg',
-    outgoingAudio: '../build/assets/audio/outgoing.ogg'
-}
-export default config
